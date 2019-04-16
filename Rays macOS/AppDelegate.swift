32494f92a3b3b1fd2feefbf3ee1937ff59327f25
Viktor Chernikov <viktorcode@icloud.com>
2019-04-16 14:54:08 +0300

    Initial Commit

---
+++ Rays macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Rays macOS
+//
+//  Created by Viktor Chernikov on 16/04/2019.
+//  Copyright © 2019 Viktor Chernikov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
